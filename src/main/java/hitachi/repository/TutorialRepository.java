de368256ac0ff718eaba20596008574eaddd507b
Sanviiz <sanviiz06@hotmail.com>
2022-06-27 14:59:15 +0700

    Initial commit

---
+++ src/main/java/hitachi/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package hitachi.repository;
+
+import hitachi.entity.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+
+  List<Tutorial> findByPublished(boolean published);
+  List<Tutorial> findByTitleContaining(String title);
+
+}
---
+++ src/test/java/hitachi/AppTest.java
@@ -0,0 +1,12 @@
+package hitachi;
+ 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+ 
+@SpringBootTest
+class AppTest {
+ 
+  @Test
+  void test01() {
+  }
+}
